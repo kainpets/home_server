ffcdda1aed4a1dbd68f10be31a936cdbe30e2823
Pawel <pawelstepniak22@gmail.com>
2025-09-27 01:36:59 +0200

    add simple ui

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,26 @@
 import Fastify, { type FastifyInstance, type RouteShorthandOptions } from 'fastify'
 import { photoRoutes } from './routes/photos'
+import path from 'path'
+import fastifyStatic from '@fastify/static'
 
 const server: FastifyInstance = Fastify({})
 
+// Register photo routes first
 server.register(photoRoutes)
+
+// Register static file serving for public files
+server.register(fastifyStatic, {
+  root: path.join(__dirname, '../public'),
+  prefix: '/',
+  decorateReply: false
+})
+
+// Serve uploaded photos
+server.register(fastifyStatic, {
+  root: path.join(__dirname, '../uploads'),
+  prefix: '/uploads/',
+  decorateReply: false
+})
 
 server.get('/ping', async (request, reply) => {
   return { pong: 'it worked!' }
